60aa7f375b78bc0db5510807d0761d01d3b3ec93
stavkoren <stavkoren123@gmail.com>
2020-08-17 18:12:04 +0300

    Create svm classification

--- svm_classification.py
+++ svm_classification.py
@@ -1,9 +1,11 @@
-#from sklearn import svm
-import prepare_data
-#from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score
+from prepare_data import split_data
+from sklearn import svm
 
 if __name__ == '__main__':
-    train_data, train_labels, test_data, test_labels = prepare_data.split_data()
+    train_data, train_labels, test_data, test_labels = split_data()
+    # create svm with C=1- no regularization
+    clf = svm.LinearSVC(loss='squared_hinge', C=1,max_iter=10)
+    scores = cross_val_score(clf, train_data, train_labels, cv=5)
+    print(scores)
     print(2);
-   # clf = svm.SVC(kernel='linear', C=1)
-    #scores = cross_val_score(clf, X, y, cv=5)
