eaebcdf05e83dea99f5ef828b7ed77bd0f9d1120
stavkoren <stavkoren123@gmail.com>
2020-08-17 12:35:53 +0300

    Initial Commit

--- svm_classification.py
+++ svm_classification.py
@@ -1,4 +1,4 @@
 import prepare_data
 
-
-train_data, train_labels, test_data, test_labels = prepare_data.split_data()
+if __name__ == '__main__':
+    train_data, train_labels, test_data, test_labels = prepare_data.split_data()
