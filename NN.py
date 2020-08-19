543986c9a25f919a7815ea543531872dec02c383
stavkoren <stavkoren123@gmail.com>
2020-08-19 22:39:18 +0300

    NN- wip plot validaton curve- wip

--- NN.py
+++ NN.py
@@ -1,50 +1,17 @@
 from plot_validation_curve import plot_validation_curve as pvc
 from sklearn.neural_network import MLPClassifier
 from prepare_data import prepare_data
-from sklearn.model_selection import learning_curve
 import matplotlib.pyplot as plt
-import numpy as np
 
-
-# plot validation curve
-def plot_Validation_curve(estimator, title, X, y, ylim, cv=None,
-                          n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
-    plt.title(title)
-    plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("Accuracy")
-
-    train_sizes, train_scores, test_scores, _, _ = \
-        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
-                       train_sizes=train_sizes,
-                       return_times=True,)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-
-    # Plot learning curve
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1,
-                         color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Validation score")
-    plt.legend(loc="best")
-    return plt
 
 
 if __name__ == '__main__':
     train_x, train_y, _, _ = prepare_data()
     title = "Learning Curves (NN)"
     # build the estimator
-    estimator = nn = MLPClassifier(hidden_layer_sizes=(25, 15, 25), max_iter=300,
-                                   activation='relu', solver='adam', alpha=0.0001,
+    estimator = nn = MLPClassifier(hidden_layer_sizes=(20, 8,20), max_iter=100,
+                                   activation='relu', solver='adam', alpha=0.01,
                                    learning_rate='constant', learning_rate_init=0.001)
-    plot_Validation_curve(estimator, title, train_x, train_y, ylim=(0.0, 1.01),cv=5,
+    pvc(estimator, title, train_x, train_y, ylim=(0.2, 0.4),cv=5,
                            n_jobs=-1)
     plt.show()
--- plot_validation_curve.py
+++ plot_validation_curve.py
@@ -4,33 +4,23 @@ import numpy as np
 
 
 # plot validation curve
-def plot_validation_curve(estimator, title, X, y, ylim=None, cv=None,
-                          n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+def plot_validation_curve(estimator, title, x, y, ylim=None, cv=None,
+                          n_jobs=None):
     plt.title(title)
     if ylim is not None:
         plt.ylim(*ylim)
     plt.xlabel("Training examples")
-    plt.ylabel("Accuracy")
+    plt.ylabel("Error")
 
-    train_sizes, train_scores, test_scores, _, _ = \
-        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
-                       train_sizes=train_sizes,
-                       return_times=True,)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-
+    train_sizes, train_scores, validation_scores, _, _ = \
+        learning_curve(estimator, x, y,train_sizes=[0.2,0.4,0.6,0.8,1.0], cv=cv, n_jobs=n_jobs,
+                       return_times=True,shuffle=True)
+    train_error_scores_mean = 1 - np.mean(train_scores, axis=1)
+    validation_error_scores_mean = 1 - np.mean(validation_scores, axis=1)
     # Plot learning curve
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1,
-                         color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Validation score")
+    plt.style.use('seaborn')
+    plt.plot(train_sizes, train_error_scores_mean, label="Training error score")
+    plt.plot(train_sizes, validation_error_scores_mean,
+             label="Validation error score")
     plt.legend(loc="best")
     return plt
