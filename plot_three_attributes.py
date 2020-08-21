bf609a5fcfd8a97a85b837287e7bba700fd5bd9f
yuvalmor <yuvalmor94@gmail.com>
2020-08-21 13:21:16 +0300

    changing the 3d plot

--- plot_three_attributes.py
+++ plot_three_attributes.py
@@ -3,19 +3,18 @@ import matplotlib.pyplot as plt
 from prepare_data import prepare_data
 
 color_converter = {1: "slategrey", 2: "lightsteelblue", 3: "cornflowerblue"}
-shape_converter = {1: "o", 2: "^", 3: "*"}
 
 train_data, train_labels, test_data, test_labels = prepare_data()
+print(train_data)
 figure = plt.figure().add_subplot(1, 1, 1, projection='3d')
 
-Length = [sample[1] for sample in train_data]
-Diameter = [sample[2] for sample in train_data]
-Whole_weight = [sample[4] for sample in train_data]
+Length = [sample[0] for sample in train_data]
+Diameter = [sample[1] for sample in train_data]
+Whole_weight = [sample[3] for sample in train_data]
 Labels = [label for label in train_labels]
 
 for i in range(len(train_data)):
-    figure.scatter(Length[i], Diameter[i], Whole_weight[i],
-                   c=color_converter.get(Labels[i]), marker=shape_converter.get(Labels[i]))
+    figure.scatter(Length[i], Diameter[i], Whole_weight[i],c=color_converter.get(Labels[i]))
 
 figure.set_xlabel('Length')
 figure.set_ylabel('Diameter')
