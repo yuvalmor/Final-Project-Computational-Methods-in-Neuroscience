0df7487f32ea520a852d6c550ef8c241eade041a
stavkoren <stavkoren123@gmail.com>
2020-08-22 14:31:45 +0300

    logistic regression -wip

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,19 +1,28 @@
 import numpy as np
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import cross_validate
+
 from prepare_data import prepare_data
 from plot_validation_curve import plot_validation_curve
 
 
 # not prepare !!! 
 
-def set_model(penalty='none', regularization_strength=1.0, solver='lbfgs', max_iterations=1000):
-    return LogisticRegression(penalty=penalty, C=regularization_strength, solver=solver, max_iter=max_iterations)
+def set_model(penalty='none', regularization_strength=1.0,
+              solver='newton-cg', max_iterations=100):
+    return LogisticRegression(penalty=penalty, C=regularization_strength,
+    multi_class='multinomial', solver=solver, max_iter=max_iterations)
 
 
-if __name__ == '__main__':
-    train_data, train_labels, test_data, test_labels = prepare_data()
-    model = set_model(penalty='l2', solver='')
-    train_data = preprocessing.scale(train_data)
-    model.fit(train_data, train_labels)
-    plot_validation_curve(model, "logistic regression", train_data, train_labels, ylim=(0.1, 0.4), cv=5)
+def lening_curve_lr(train_x, train_y):
+    model = set_model()
+    # model.fit(train_x,train_y)
+    # y_pred= model.predict(train_x)
+    # print(accuracy_score(train_y,y_pred))
+    # scores = cross_val_score(model,train_x,train_y,cv=5)
+    # print(scores)
+    plot_validation_curve(model, "logistic regression", train_x, train_y, ylim=(0.1, 1.0),
+                         )
