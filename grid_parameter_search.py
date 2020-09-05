961f65bf1055aafade32f84d36ed47fe7e86a6e6
yuvalmor <yuvalmor94@gmail.com>
2020-09-05 08:34:46 +0300

    wip

--- grid_parameter_search.py
+++ grid_parameter_search.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import GridSearchCV
 
-def grid_search(estimator,param_grid,train_x,train_y):
-    clf= GridSearchCV(estimator, param_grid)
-    clf.fit(train_x,train_y)
+
+def grid_search(estimator, param_grid, train_x, train_y):
+    clf = GridSearchCV(estimator, param_grid)
+    clf.fit(train_x, train_y)
     print("The best parameters are:")
     print(clf.best_params_)
