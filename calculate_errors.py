c5f0db32ed4ccfb66149a77c9c4b324cb3efbc0e
yuvalmor <yuvalmor94@gmail.com>
2020-09-09 11:30:18 +0300

    SVM model - done

--- calculate_errors.py
+++ calculate_errors.py
@@ -4,13 +4,14 @@ import constants
 
 
 def calculate_error_with_cross_validation(train_data, train_labels, model):
-    cv_results = cross_validate(model, train_data, train_labels, return_train_score=True)
+    cv_results = cross_validate(model, train_data, train_labels, return_train_score=True, cv=5)
     return 1-cv_results['train_score'], 1-cv_results['test_score']
 
 
 def calculate_error_for_different_training_sizes(train_data, train_labels, validation_data, validation_labels, model):
     train_error_list = []
     validation_error_list = []
+
     for size in constants.TRAIN_SIZES:
         error_train, error_validation = calculate_error(train_data[:size], train_labels[:size],
                                                         validation_data, validation_labels, model)
@@ -20,8 +21,11 @@ def calculate_error_for_different_training_sizes(train_data, train_labels, valid
 
 
 def calculate_error(train_x, train_y, validation_x, validation_y, model):
+    # Train the model
     model.fit(train_x, train_y)
+    # Prediction on training set
     train_y_hat = model.predict(train_x)
+    # Prediction on validation set
     validation_y_hat = model.predict(validation_x)
     error_train = 1 - accuracy_score(train_y_hat, train_y)
     error_validation = 1 - accuracy_score(validation_y_hat, validation_y)
--- grid_parameter_search.py
+++ grid_parameter_search.py
@@ -1,24 +1,28 @@
 from sklearn.model_selection import GridSearchCV
 import numpy as np
-from sklearn.utils.testing import ignore_warnings
-from sklearn.exceptions import ConvergenceWarning
+import constants
 
-@ignore_warnings(category=ConvergenceWarning)
+
 def grid_search(estimator, param_grid, train_x, train_y):
     clf = GridSearchCV(estimator, param_grid)
     clf.fit(train_x, train_y)
-    print("The best parameters are:")
+    print("The optimal parameters are:")
     print(clf.best_params_)
+    print("The best accuracy is:")
     print(clf.best_score_)
 
+
 def get_svm_param_grid():
-    return {'kernel': ('linear','poly','rbf','sigmoid'),'max_iter': [50, 100, 150, 200,500,700,1000],
-            'C': np.logspace(-4,4,20)}
+    return {'kernel': constants.KERNEL_PARAMETERS,
+            'gamma': constants.GAMMA_PARAMETERS,
+            'C': constants.C_PARAMETERS}
+
 
 def get_decision_tree_grid():
     return {'criterion':('gini','entropy'),
             'max_depth':[2,5,0,15,20],
             'min_samples_split':[5,10,15,20,30,50]}
+
 
 def get_logitic_regression_grid():
     return {'penalty':('l1','l2'),
